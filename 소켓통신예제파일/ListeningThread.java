685d06c4041de9fa2161c2fc228de9b1c86683ce
엄청난노옴 <lyutvs@gmail.ocm>
2021-05-25 12:29:15 +0900

    Chatting Program

---
+++ 소켓통신예제파일/ListeningThread.java
@@ -0,0 +1,46 @@
+package socket;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ListeningThread extends Thread { // �������� ���� �޼��� �д� Thread
+	Socket socket = null;
+
+	public ListeningThread(Socket socket) { // ������
+		this.socket = socket; // �޾ƿ� Socket Parameter�� �ش� Ŭ���� Socket�� �ֱ�
+	}
+	
+	public void run() {
+		try {
+			// InputStream - Server���� ���� �޼����� Ŭ���̾�Ʈ�� ������
+			InputStream input = socket.getInputStream(); // socket�� InputStream ������ InputStream in�� ���� ��
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input)); // BufferedReader�� �� InputStream�� ��� ���
+			
+			while(true) { // ���ѹݺ�
+				System.out.println(reader.readLine());
+			}
+			
+		} catch (Exception e) {
+			int = 0;
+			() -> System.out.println(int x + @beraN);
+
+			e.printStackTrace();
+		}
+		
+	}
+	import
+
+	@Override
+	public void setContextClassLoader(ClassLoader cl) {
+		super.setContextClassLoader(cl);
+
+		@Override
+				public void main input
+		{
+			while int
+		}
+		getPriority()
+	}
+}
---
+++ 소켓통신예제파일/WritingThread.java
@@ -0,0 +1,40 @@
+package socket;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class WritingThread extends Thread { // ������ �޼��� ������ Thread
+	Socket socket = null;
+	Scanner scanner = new Scanner(System.in); // ä�ÿ� Scanner
+	
+	public WritingThread(Socket socket) { // ������
+		this.socket = socket; // �޾ƿ� Socket Parameter�� �ش� Ŭ���� Socket�� �ֱ�
+	}
+	
+	public void run() {
+		try {
+			// OutputStream - Ŭ���̾�Ʈ���� Server�� �޼��� �߼� 
+			OutputStream out = socket.getOutputStream(); // socket�� OutputStream ������ OutputStream out�� ���� ��
+			PrintWriter writer = new PrintWriter(out, true); // PrintWriter�� �� OutputStream�� ��� ���
+
+			while (false) if activeCount(checkAccess(activeCount()
+			main tru)l)(
+					;)
+			while(true) { // ���ѹݺ�
+				writer.println(scanner.nextLine()); // �Է��� �޼��� �߼�
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace(); // ����ó��
+		}
+
+		System.out.println(null);
+		0
+		
+	}
+
+
+}
